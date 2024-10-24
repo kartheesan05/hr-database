3faf66dbe2bc9e9d6a9a7cf7521b57f8352ddbb6
Kartheesan69 <2022cs0138@svce.ac.in>
2024-10-24 15:47:30 +0530

    added metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,11 @@
 import "./globals.css";
 import "./custom.scss";
 
+export const metadata = {
+  title: "HR Database",
+  description: "HR Database for Forese Task",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
