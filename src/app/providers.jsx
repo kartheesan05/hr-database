dfbe3e498e9fda0df37328da14035883d53aa170
kartheesan05 <kartheesan2005@gmail.com>
2025-10-08 11:43:20 +0530

    removed drizzle

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -8,7 +8,7 @@ import PostHogPageView from "./PostHogPageView";
 
 export function PostHogProvider({ children }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "phc_riPwEwyU7lDiadmxJvzgOWS64vT3BRC6zz69dhg4nQg", {
       api_host: "/ingest",
       ui_host: "https://us.posthog.com",
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
